0fce7d030499b93667e85159bf3ef1ba84a1c0de
Unknown <gdelong1@mail.csuchico.edu>
2017-03-30 22:03:28 -0700

    Stage 3

--- test.js
+++ test.js
@@ -1,7 +1,27 @@
+// Author: Garrett De Long
 function correctPattern()
 {
-  var answer = document.getElementById("pat")
-  if(answer.value == 3)
+  var answer = document.getElementById("pat").value;
+  if(answer == 34)
+  {
+    document.getElementById("err").innerHTML = "correct";
+  }
+  else if(isNaN(answer.value))
+  {
+    console.log(answer);
+    console.log("incorrect");
+    document.getElementById("err").innerHTML = "incorrect";
+  }
+  else
+    {
+      document.getElementById("err").innerHTML = "Enter a number";
+    }
+}
+
+function correctAddition()
+{
+  var answer = document.getElementById("pat").value;
+  if(answer == 571792992)
   {
     document.getElementById("err").innerHTML = "correct";
   }
@@ -13,4 +33,26 @@ function correctPattern()
     {
       document.getElementById("err").innerHTML = "Enter a number";
     }
+}
+
+function correctContractions()
+{
+  var answer = document.getElementById("pat").value;
+  if(answer == "mustn't")
+  {
+    document.getElementById("err").innerHTML = "correct";
+  }
+  else if(isNaN(answer.value))
+  {
+    document.getElementById("err").innerHTML = "Enter a word";
+  }
+  else
+    {
+      document.getElementById("err").innerHTML = "Incorrect";
+    }
+}
+
+function Results()
+{
+
 }
